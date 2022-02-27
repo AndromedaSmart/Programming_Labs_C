805638d2771f24dbcb64934c482dbbdcddfd98ac
AndromedaSmart <80599498+AndromedaSmart@users.noreply.github.com>
2022-02-27 04:12:18 +0300

    Create triangle.h

---
+++ 3rd_block/Lab11/triangle.h
@@ -0,0 +1,13 @@
+#ifndef TRIANGLE_H_INCLUDED
+#define TRIANGLE_H_INCLUDED
+
+#include "point.h"
+
+struct Triangle
+{
+    struct point A;
+    struct point B;
+    struct point C;
+};
+
+#endif // TRIANGLE_H_INCLUDED
